9a5a43eb1d6a8845ac5a231bcb0e005b82813add
Kevin Carbeti <kevincarbeti@gmail.com>
2022-09-01 10:34:47 +0200

    first commit

---
+++ src/main/java/com/simplon/course_voilier/CourseVoilierApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.course_voilier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseVoilierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseVoilierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplon/course_voilier/CourseVoilierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.course_voilier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseVoilierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
