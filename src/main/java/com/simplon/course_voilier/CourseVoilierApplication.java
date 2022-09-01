4a11b94b2007165e210966dac6381ae5d88c0bf0
Kevin Carbeti <kevincarbeti@gmail.com>
2022-09-01 15:17:25 +0200

    encryptorBean

--- src/main/java/com/simplon/course_voilier/CourseVoilierApplication.java
+++ src/main/java/com/simplon/course_voilier/CourseVoilierApplication.java
@@ -1,7 +1,11 @@
 package com.simplon.course_voilier;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
@@ -12,5 +16,19 @@ public class CourseVoilierApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseVoilierApplication.class, args);
 	}
-
+	
+	@Bean(name="encryptorBean")
+	public StringEncryptor stringEncryptor() {
+	    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+	    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+	    config.setPassword("Overwatch");
+	    config.setAlgorithm("PBEWithMD5AndDES");
+	    config.setKeyObtentionIterations("1000");
+	    config.setPoolSize("1");
+	    config.setProviderName("SunJCE");
+	    config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+	    config.setStringOutputType("base64");
+	    encryptor.setConfig(config);
+	    return encryptor;
+	}
 }
