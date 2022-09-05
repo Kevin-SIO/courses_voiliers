38aa70ac090d3673c48147ac1a0384d373e4aa50
Kevin Carbeti <kevincarbeti@gmail.com>
2022-09-05 10:12:19 +0200

    hide new password

--- src/main/java/com/simplon/course_voilier/CourseVoilierApplication.java
+++ src/main/java/com/simplon/course_voilier/CourseVoilierApplication.java
@@ -1,27 +1,41 @@
 package com.simplon.course_voilier;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableEncryptableProperties
-public class CourseVoilierApplication {
+@PropertySource("classpath:application.properties")
+public class CourseVoilierApplication{
+	
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseVoilierApplication.class, args);
+		SpringApplication.run(CourseVoilierApplication.class, args);	
 	}
 	
 	@Bean(name="encryptorBean")
 	public StringEncryptor stringEncryptor() {
+		
 	    PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 	    SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-	    config.setPassword("Overwatch");
+	    
+	    config.setPassword(env.getProperty("password"));
 	    config.setAlgorithm("PBEWithMD5AndDES");
 	    config.setKeyObtentionIterations("1000");
 	    config.setPoolSize("1");
