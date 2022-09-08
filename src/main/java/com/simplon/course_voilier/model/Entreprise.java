d7b48d9bb19a8a2547a2f3f3283e1cc1b19abdbe
Kevin Carbeti <kevincarbeti@gmail.com>
2022-09-08 10:59:05 +0200

    récup nom voilier + debut MEP

--- src/main/java/com/simplon/course_voilier/model/Entreprise.java
+++ src/main/java/com/simplon/course_voilier/model/Entreprise.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -21,13 +22,14 @@ public class Entreprise {
 	@Column(name="id_entreprise")
 	private int id;
 	
-	@Column(name="nom_entreprise")
+	@Column(name = "nom_entreprise")
 	private String nom;
 	
 	private String logo;
 	
-//	@ManyToOne
-//	private Voilier voilier;
+	@ManyToOne
+	@JoinColumn(name = "id_voilier", referencedColumnName = "id_voilier")
+	private Voilier voilier;
 	
 
 }
--- src/main/java/com/simplon/course_voilier/model/Voilier.java
+++ src/main/java/com/simplon/course_voilier/model/Voilier.java
@@ -1,11 +1,14 @@
 package com.simplon.course_voilier.model;
 
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -23,4 +26,6 @@ public class Voilier {
 	private String nom;
 	private int longeur;
 	private int largeur;
+	@OneToMany(mappedBy = "voilier")
+	private List<Entreprise> entreprise;
 }
