582cc67062374e0f74c10e9497c6af00a80b5454
Kevin Carbeti <kevincarbeti@gmail.com>
2022-09-11 09:58:22 +0200

    initialisation session

--- src/main/java/com/simplon/course_voilier/controller/AdminController.java
+++ src/main/java/com/simplon/course_voilier/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.simplon.course_voilier.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,29 +21,43 @@ public class AdminController {
 	AdminService as;
 	
 	@GetMapping("/admin")
-	public String connexionAdmin(Model model) {
-//		if(message.equals("erreur")) {
-//			model.addAttribute("message","Identifiant et/ou mot de passe incorrect");
-//		}
-//		else if(message.equals("deconnexion")){
-//			model.addAttribute("message", "Deconnexion effectué");
-//		}
-		
-		model.addAttribute("admin", new Admin());
-//		
+	public String connexionAdmin(Model model) {	
+		model.addAttribute("admin", new Admin());		
 		return "connexion";
 	}
 	
 	@PostMapping("/admin")
-	public String connexionAdmin(@ModelAttribute Admin admin, Model model) {
+	public String connexionAdmin(@ModelAttribute Admin admin, Model model, HttpServletResponse response) {
+		
 		if(as.connexion(admin.getIdentifiant(), admin.getMdp()) == null) {
 			model.addAttribute("erreur", "Identifiant et/ou mot de passe incorrect");
 			return "connexion";
 		}
 		else {
-			//TODO creation de session
+			
+			Cookie cookie = new Cookie("identifiant", admin.getIdentifiant());
+			cookie.setHttpOnly(true);
+			
+			response.addCookie(cookie);
+			
 			return "redirect:/admin/index";
 		}
 	}
+	
+	@GetMapping("/admin/index")
+	public String accueilAdmin(Model model, @CookieValue(value = "identifiant", defaultValue = "none") String id) {
+		
+		if(id.equals("none")) {
+			
+			model.addAttribute("erreur", "Vous devez être connecté pour accéder à cette partie du site");
+			return "connexion";
+			
+		} else {
+			
+			model.addAttribute("identifiant", id);
+			return "accueil_admin";
+		}
+	}
+	
 	
 }
